0790756a3cb1805ed3ba9b78c0d161257228bdc4
JazimLatif <74251072+JazimLatif@users.noreply.github.com>
2024-06-18 01:06:17 +0100

    initial commit

---
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -0,0 +1,18 @@
+package com.example.wishlistapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.wishlistapp.ui.theme.AppBarView
+
+@Composable
+fun HomeView() {
+    Scaffold(
+        topBar = { AppBarView(title= "WishList") }
+    ){
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) { }
+    }
+}
---
+++ app/src/main/java/com/example/wishlistapp/ui/theme/AppBar.kt
@@ -0,0 +1,33 @@
+package com.example.wishlistapp.ui.theme
+
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.wishlistapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBarView(
+    title: String,
+    onBackNavClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = colorResource(id = R.color.white),
+                modifier = Modifier
+                    .padding(start = 4.dp)
+                    .heightIn(max = 24.dp)
+            )
+        },
+        elevation = 3.dp,
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+    )
+}
